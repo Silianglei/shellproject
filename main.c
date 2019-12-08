daa8666720ee2e1a7fabad1b5d4fb08cb03d9a0c
Akash Das <akashdas.ny@gmail.com>
2019-12-07 23:16:14 -0500

    added stuff

--- main.c
+++ main.c
@@ -17,30 +17,87 @@ char ** parse_args( char * line ){
   int i = 0;
   while(current != NULL){
     args[i] = strsep(&current, " ");
-    //args[i] = token;
     i++;
   }
   args[i] = NULL;
   return args;
 }
 
+void runCommand2(int j, int k, char input[]){
+  input[strlen(input)-1] = 0;
+  char * line = input;
+  char ** args = parse_args( line );
+  k = fork();
+  if(k==0) {
+    execvp(args[0], args);
+  }
+  wait(&j);
+}
+
+void runCommand(int j, int k){
+  char input[100];
+  fgets(input, sizeof(input), stdin);
+  runCommand2(j, k, input);
+  // input[strlen(input)-1] = 0;
+  // char * line = input;
+  // char ** args = parse_args( line );
+  // k = fork();
+  // if(k==0) {
+  //   execvp(args[0], args);
+  // }
+  // wait(&j);
+}
+
+
+
+char ** parse_lines( char * lines ){
+  char *current = lines;
+  char *token;
+  char ** args = malloc(10 * sizeof(char*));
+  int i = 0;
+  while(current != NULL){
+    args[i] = strsep(&current, ";");\
+    if (i > 0) {
+      args[i]++;
+    }
+    i++;
+  }
+  args[i] = NULL;
+  return args;
+}
+
+void runMult(int j, int k){
+  char input[100];
+  fgets(input, sizeof(input), stdin);
+  char ** commandsToRun = parse_lines(input);
+  int i = 0;
+  while(commandsToRun[i] != NULL){
+    //printf("'%s'\n", commandsToRun[i]);
+    runCommand2(j, k, commandsToRun[i]);
+    i++;
+  }
+}
+
+
+
 int main(){
-  // printf("Input Your Command:");
   int j;
   int k;
   while(1==1){
     printf("Input Your Command:");
-    char input[100];
-    fgets(input, sizeof(input), stdin);
-    input[strlen(input)-1] = 0;
-    char * line = input;
-    //printf("%s\n", line );
-    char ** args = parse_args( line );
-    k = fork();
-    if(k==0) {
-      execvp(args[0], args);
-    }
-    wait(&j);
+    // char input[100];
+    // fgets(input, sizeof(input), stdin);
+    // input[strlen(input)-1] = 0;
+    // char * line = input;
+    // //printf("%s\n", line );
+    // char ** args = parse_args( line );
+    // k = fork();
+    // if(k==0) {
+    //   execvp(args[0], args);
+    // }
+    // wait(&j);
+    char token[100] = "ls -a -l";
+    runMult(j,k);
   }
   return 0;
 }
